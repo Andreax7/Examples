fea29dc04e47bae392d699be350092253bce6312
Andreax7 <andreaatx7@gmail.com>
2023-02-16 13:50:11 +0100

    del

--- cinema_koa_react/backend/database/_queries/categories.js
+++
@@ -1,9 +0,0 @@
-const {pool} = require('../connection.js')
-
-async function getCategories() {
-  return pool.query('SELECT * FROM category')
-}
-
-module.exports = {
-    getCategories
-}
--- cinema_koa_react/frontend_cinemas/src/auth/RequireAuth.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
-
-export const RequireAuth = ({ children }) => { 
-    const userDecoded = localStorage.getItem('Basic ');
-    console.log('req auth ', userDecoded )
-    return userDecoded ? (children) : (<Navigate to="/"/> );
-};
